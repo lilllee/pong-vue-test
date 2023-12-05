3cccd44bd49bf84460393149628dda166aaa97f1
LillLEE <kopqwe147@gmail.com>
2023-12-05 15:11:18 +0900

    add header footer

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
 <template>
-  <div id="app" class="container">
-    <ProfileCard data-image="mode.png" header-text="Mode" header-content="일반 모드입니다.">
-    </ProfileCard>
-    <ProfileCard data-image="learderboard.png" header-text="Board" header-content="정보를 확인할수 있는 리더 보드입니다.">
-    </ProfileCard>
-    <ProfileCard data-image="multi.png" header-text="Multi" header-content="멀티플레이 할수있는 모드입니다.">
-    </ProfileCard>
-    <ProfileCard data-image="single.png" header-text="Single" header-content="싱글플레이를 할수있는 모드입니다.">
-    </ProfileCard>
+  <div>
+      <HeaderView></HeaderView>
+      <div id="app" class="container">
+        <ProfileCard data-image="mode.png" header-text="Mode" header-content="일반 모드입니다.">
+        </ProfileCard>
+        <ProfileCard data-image="learderboard.png" header-text="Board" header-content="정보를 확인할수 있는 리더 보드입니다.">
+        </ProfileCard>
+        <ProfileCard data-image="multi.png" header-text="Multi" header-content="멀티플레이 할수있는 모드입니다.">
+        </ProfileCard>
+        <ProfileCard data-image="single.png" header-text="Single" header-content="싱글플레이를 할수있는 모드입니다.">
+        </ProfileCard>
+      </div>
+      <FooterView></FooterView>
   </div>
 </template>
 
 <script>
 import ProfileCard from './components/ProfileCard.vue';
+import HeaderView from './components/HeaderView.vue'
+import FooterView from './components/FooterView.vue'
 
 export default {
   name: 'App',
   components: {
     ProfileCard
+    , HeaderView
+    , FooterView
   }
 }
 </script>
@@ -29,4 +37,14 @@ export default {
   flex-wrap: wrap;
   justify-content: center;
 }
+
+header {
+	background: #707070;
+	grid-area: header;
+}
+footer {
+	background: #707070;
+	grid-area: footer;
+}
+
 </style>
